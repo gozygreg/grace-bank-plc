bf66735a8415c176d1415eff15e86080c07340fa
gozygreg <goziennabuife@yahoo.com>
2022-06-26 19:53:41 +0000

    Create logo using pyfiget, import time module and use this to create a delay function. Also create function that welcomes customers

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ or method within it
 """
 import gspread
 from google.oauth2.service_account import Credentials
+# importing time module that will be used to delay some code execution
+import time
+# Create logo using pyfiglet
+import pyfiglet
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,3 +25,30 @@ customers = SHEET.worksheet('customers')
 data = customers.get_all_values()
 
 print(data)
+
+
+def code_execution_delay(information):
+    """
+    Function to delay execution of some code and allow good
+    flow of the program
+    """
+    print(information)
+    time.sleep(1.5)
+
+
+def welcome_to_gracebank():
+    """
+    Welcoming customers to Grace Bank Plc
+    """
+    gb_logo = pyfiglet.figlet_format("GRACE BANK Plc")
+    print(gb_logo)
+    print("Hi there! Welcome to Grace Bank Plc.\n")
+    print("...the Bank of champions")
+    code_execution_delay("press ANY button to start....\n")
+    input("")
+    code_execution_delay(
+        "\nGBPlc-GBPlc-GBPlc------GBPlc-GBPlc-GBPlc-GBPlc"
+        "------GBPlc-GBPlc-GBPlc------\n")
+
+
+welcome_to_gracebank()
